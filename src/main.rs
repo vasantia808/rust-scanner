f1c5b4b9f261411abc0bdeac390e7b2f489dafb2
vasantia808 <vasantia@yahoo.com>
2026-07-15 15:17:26 -0700

    Add Config struct closes #4

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,20 @@ enum OutputFormat {
     PlainText,
 }
 
+struct Config {
+    target: String,
+    start_port: u16,
+    end_port: u16,
+    scan_type: ScanType,
+    output_format: OutputFormat,
+    output_file: Option<String>,
+    timeout_ms: u16,
+    max_concurrent: u16,
+    verbose: bool,
+    notes: Option<String>,
+    target_name: Option<String>,
+}
+
 fn main() {
    
 }
